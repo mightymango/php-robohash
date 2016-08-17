a88ea4e485228585c32933b6a2a7d7dc597671b6
Simon Barrett <simonbarrett@me.com>
2016-08-17 13:27:59 +0100

    Update Robohash.php

--- lib/Robohash.php
+++ lib/Robohash.php
@@ -268,27 +268,27 @@ class Robohash
         return self::$bgsets[array_rand(self::$bgsets)];
     }
 
-/** 
- * my_bcmod - get modulus (substitute for bcmod) 
- * string my_bcmod ( string left_operand, int modulus ) 
- * left_operand can be really big, but be carefull with modulus :( 
- * by Andrius Baranauskas and Laurynas Butkus :) Vilnius, Lithuania 
- **/ 
-static function my_bcmod( $x, $y ) 
-{ 
-    // how many numbers to take at once? carefull not to exceed (int) 
-    $take = 5;     
-    $mod = ''; 
-
-    do 
+    /** 
+     * my_bcmod - get modulus (substitute for bcmod) 
+     * string my_bcmod ( string left_operand, int modulus ) 
+     * left_operand can be really big, but be carefull with modulus :( 
+     * by Andrius Baranauskas and Laurynas Butkus :) Vilnius, Lithuania 
+     **/ 
+    function my_bcmod( $x, $y ) 
     { 
-        $a = (int)$mod.substr( $x, 0, $take ); 
-        $x = substr( $x, $take ); 
-        $mod = $a % $y;    
+        // how many numbers to take at once? carefull not to exceed (int) 
+        $take = 5;     
+        $mod = ''; 
+    
+        do 
+        { 
+            $a = (int)$mod.substr( $x, 0, $take ); 
+            $x = substr( $x, $take ); 
+            $mod = $a % $y;    
+        } 
+        while ( strlen($x) ); 
+    
+        return (int)$mod; 
     } 
-    while ( strlen($x) ); 
-
-    return (int)$mod; 
-} 
 
 }
